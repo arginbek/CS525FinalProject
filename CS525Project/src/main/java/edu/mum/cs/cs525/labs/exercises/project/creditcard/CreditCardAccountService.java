561bc5055f10b47eda370deb4ddbf475a5039f99
arginbek <arginbek@gmail.com>
2017-10-25 20:23:43 -0500

    report

--- CS525Project/src/main/java/edu/mum/cs/cs525/labs/exercises/project/creditcard/CreditCardAccountService.java
+++ CS525Project/src/main/java/edu/mum/cs/cs525/labs/exercises/project/creditcard/CreditCardAccountService.java
@@ -41,7 +41,7 @@ public class CreditCardAccountService extends AccountServiceImpl {
 	}
 
 	public List<Report> getReport() {
-		LocalDate lastMonth = LocalDate.now().minusMonths(1);
+		LocalDate lastMonth = LocalDate.now();
 		LocalDate start = lastMonth.withDayOfMonth(1);
 		LocalDate end = lastMonth.withDayOfMonth(lastMonth.lengthOfMonth());
 		List<Report> report = new ArrayList<>();
